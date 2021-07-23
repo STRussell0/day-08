7662eaf0c6860b923c42269f019face17a5a6b3d
Stephen Russell <srussell@alphaworks.tech>
2021-07-23 07:04:04 -0700

    Update: Comments on take-home.js

--- js/take-home.js
+++ js/take-home.js
@@ -1,6 +1,6 @@
-class car {
+class car { //created a class named car
 
-    constructor (make, model, year, color) {
+    constructor (make, model, year, color) { //constructor used
         this.make = make;
         this.model = model;
         this.year = year;
@@ -11,15 +11,18 @@ class car {
  const cars = [new car('Chevy', 'Cobalt', 2009, 'White'), new car('Mitsubishi', 'Diamante', 1994, 'White'), 
  new car('Toyota', 'Camry', 1995, 'Gold'),  new car('Toyota', 'T-100', 1998, 'Blue')];
 
- console.log(cars);
+ //created a cars array with new car objects
 
- const listElement = document.createElement('ul');
+ console.log(cars); //verifying contents of cars array
+
+ const listElement = document.createElement('ul'); // created an unordered list and appended it to the html doc
  document.body.appendChild(listElement);
 
-cars.forEach( (car) => {
-    const li = document.createElement('li');
+cars.forEach( (car) => { //for each item in the cars array...
+    const li = document.createElement('li'); //...create a list item and append it to the doc
     listElement.appendChild(li);
-    li.innerText = `${car.color} ${car.year} ${car.make} ${car.model}
+    //the inner text of each list item is each objects property
+    li.innerText = `${car.color} ${car.year} ${car.make} ${car.model} 
 
     `;
 })
