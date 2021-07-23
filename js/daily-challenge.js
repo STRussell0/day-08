639e0f1be6462aee77923f03811f619b9d42ba14
Stephen Russell <srussell@alphaworks.tech>
2021-07-23 06:58:17 -0700

    Update: Comments on daily-challenge.js

--- js/daily-challenge.js
+++ js/daily-challenge.js
@@ -1,15 +1,15 @@
-const dog = {
-    name: 'Fluffy',
+const dog = { //created an object named dog
+    name: 'Fluffy', //gave it a few properties
     legs: 5,
     furColor: 'yes',
-    speak: function() {
+    speak: function() { //created a function so it can speak when called upon
         alert('Woof!');
     },
-    speakYourName: function () {
+    speakYourName: function () { //second function to call upon
         alert('Hi, my name is ' + this.name + '!');
     }
 };
 
-// dog.speakYourName();
+// dog.speakYourName(); //called the second function in the object
 
-console.log(dog);
+console.log(dog); //verifying the contents of dog
